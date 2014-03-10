09fbf938bca44e82e15619b8adacc3a1f9edcc33
zigolis <gabriel.zigolis@gmail.com>
2014-03-09 22:48:28 -0300

    Integration test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,19 +67,9 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Test CodeIgniter +++ Git +++ GitHub </h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+	<img src="http://2.bp.blogspot.com/-1XGFDmwZpKM/UMsBdYTfxHI/AAAAAAAAJEM/n8UvB2nojR8/s400/inter-mazembe-day.jpg" />
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
